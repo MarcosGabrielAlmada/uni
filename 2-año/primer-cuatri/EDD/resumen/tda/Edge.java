f41ddacb0cbdcfbe8d469bd69dd874ac07d9d69e
Marcos <marcosalmada1067@gmail.com>
2026-05-14 12:29:21 -0300

    sync

---
+++ 2-año/primer-cuatri/EDD/resumen/tda/Edge.java
@@ -0,0 +1,9 @@
+package tda;
+
+/**
+ * Interface Edge
+ * @author Cátedra de Estructuras de Datos, Departamento de Cs. e Ing. de la Computación, UNS.
+ */
+public interface Edge<E> extends Position<E> {
+	
+}
---
+++ 2-año/primer-cuatri/EDD/resumen/tda/Vertex.java
@@ -0,0 +1,9 @@
+package tda;
+
+/**
+ * Interface Vertex
+ * @author Cátedra de Estructuras de Datos, Departamento de Cs. e Ing. de la Computación, UNS.
+ */
+public interface Vertex<V> extends Position<V> {
+	
+}
