60ad113bb0620d8c6afec48c6e5b848bee6e729d
Marcos <marcosalmada1967@gamil.com>
2026-05-17 20:15:10 -0300

    sync

--- 2-año/primer-cuatri/EDD/trabajos-practicos/src/main/java/ar/edu/uns/cs/ed/tdas/tps/tp7/BinaryTreeTests.java
+++ 2-año/primer-cuatri/EDD/trabajos-practicos/src/main/java/ar/edu/uns/cs/ed/tdas/tps/tp7/BinaryTreeTests.java
@@ -41,11 +41,11 @@ public class BinaryTreeTests {
 	 */
 	@Before
 	public void setUp() {
-		//binaryTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
+		binaryTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
 		
 		// para método attach
-		//leftTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
-		//rightTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
+		leftTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
+		rightTree = new ArbolBinario<Object>(); //TODO: Descomentar y crear instancia del TDA a testear
 		
 		o1 = new Object();
 		o2 = new Object();
